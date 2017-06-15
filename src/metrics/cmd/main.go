6cfe84d176538fab5afcd57b33ddba0279d26a90
Ryan Day <rynday@amazon.com>
2017-06-15 18:35:43 +0000

    Add cli command

---
+++ src/metrics/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"metrics"
+	"time"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"fmt"
+)
+
+func main() {
+	sess := session.Must(session.NewSession())
+	svc := cloudwatch.New(sess)
+	dimensions := []*cloudwatch.Dimension{
+		{
+			Name:  aws.String("DimensionName"),
+			Value: aws.String("DimensionValue"),
+		},
+	}
+
+	metric := metrics.NewCustomMetric("Namespace", "Metric", dimensions)
+	ch, doneCh := metrics.CollectMetrics(metric, svc, 10)
+	ch <- 1.0
+	ch<- 2.0
+	ch<-3.0
+	fmt.Println("Sleep")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Done")
+
+	ackCh := make(chan struct{}, 1)
+	doneCh<-ackCh
+	<-ackCh
+
+}
